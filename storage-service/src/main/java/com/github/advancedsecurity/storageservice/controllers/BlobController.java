4ad0891454f00479d297de379a6d5c7a4950e98d
coveros-phil <114023548+coveros-phil@users.noreply.github.com>
2022-10-05 21:03:57 -0400

    Update BlobController.java

--- storage-service/src/main/java/com/github/advancedsecurity/storageservice/controllers/BlobController.java
+++ storage-service/src/main/java/com/github/advancedsecurity/storageservice/controllers/BlobController.java
@@ -40,7 +40,12 @@ public class BlobController {
     @Value("${blob.allowed-content-types}")
     private String[] allowedContentTypes;
 
-    
+    private Blob deserializeBlob(InputStream inputStream) throws IOException, ClassNotFoundException{
+        ObjectInputStream in = new ObjectInputStream(inputStream);
+        Blob b = new Blob();//(Blob)in.readObject();
+        in.close();
+        return b;
+    }
 
     @GetMapping("/blob/{id}")
     public Blob get(@PathVariable UUID id, JwtAuthenticationToken token) {
